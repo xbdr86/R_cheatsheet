fd4a7ed349226136a98e7b2a46713b445d696284
Xavier <xbdr86@gmail.com>
2020-04-07 14:53:36 -0400

    Update tips.R

--- tips.R
+++ tips.R
@@ -3,3 +3,6 @@ d[is.na(d)] <- 0
 
 #Pull function from other R script
 source("~/Desktop/R_scripts/universal_names_function.R")
+
+#Substitute exact match
+gsub("\\<AC093012.1\\>","IRAK4",external_gene_name)
