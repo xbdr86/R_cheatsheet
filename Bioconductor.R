d7dd50789536fe132534a1b5a806a35cc2f227f0
Xavier <xbdr86@gmail.com>
2020-05-03 16:12:58 -0400

    Update Bioconductor.R

--- Bioconductor.R
+++ Bioconductor.R
@@ -25,3 +25,43 @@ nchar(yeastGenome$chrM)
 
 getSeq(yeastGenome, names = "chrI", start = 100, end = 150)
 getSeq(yeastGenome, end=30) # Get the first 30 bases of each chromosome
+
+# Check the alphabet of the zikaVirus
+alphabet(zikaVirus)
+
+# Check the alphabetFrequency of the zikaVirus
+alphabetFrequency(zikaVirus)
+# Check alphabet of the zikaVirus using baseOnly = TRUE
+alphabet(zikaVirus, baseOnly = TRUE)
+readDNAStringSet(zikaVirus)
+
+# Unlist the set, select the first 21 letters, and assign to dna_seq
+dna_seq <- subseq(unlist(zikaVirus), end = 21)
+dna_seq
+
+# Transcribe dna_seq into an RNAString object and print it
+rna_seq <- RNAString(dna_seq) 
+
+# Translate rna_seq into an AAString object and print it
+aa_seq <- translate(rna_seq)
+
+# read the sequence as a set
+zikaVirus <- readDNAStringSet("data/zika.fa")
+# Create zikv with one collated sequence using `zikaVirus`
+zikv <- unlist(zikaVirus)
+
+# Check the length of zikaVirus and zikv
+length(zikaVirus)
+length(zikv)
+
+# Reverse the zikv sequence
+reverse(zikv)
+
+# Complement the zikv sequence
+complement(zikv)
+
+# Reverse complement the zikv sequence
+reverseComplement(zikv)
+
+# Translate the zikv sequence
+translate(zikv)
