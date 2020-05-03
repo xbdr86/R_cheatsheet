286e1a87249384f919012691bfac834c5b016b51
Xavier <xbdr86@gmail.com>
2020-05-03 11:33:04 -0400

    Update Bioconductor.R

--- Bioconductor.R
+++ Bioconductor.R
@@ -1,1 +1,9 @@
 
+library(BiocInstaller) # Load the BiocInstaller packag
+version # Check R version
+
+BiocInstaller::biocVersion() # Check Bioconductor version (For versions <= 3.7)
+# or biocVersion()
+
+library(BSgenome) #Load the BSgenome package
+packageVersion("BSgenome") #Check the version of the BSgenome package
