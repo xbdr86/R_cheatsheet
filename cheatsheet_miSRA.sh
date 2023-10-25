42278c73703283b929976aaf19efe0a900f5167d
Xavier <xbdr86@gmail.com>
2023-10-25 13:57:06 +0200

    Update cheatsheet_miSRA.sh

--- cheatsheet_miSRA.sh
+++ cheatsheet_miSRA.sh
@@ -13,7 +13,10 @@ mkdir miSRA_example
 cd miSRA_example  
 miSRA -c config.json
 #miSRA --config config.json
+miSRA -c config.json
 
+conda activate miSRA 
+cd Desktop/R_scripts/isomiRdb/miSRA/miR_222_3p 
 
 # mv hairpin_hsa.fa.txt hairpin_hsa.fa
 # mv mature_hsa.fa.txt mature_hsa.fa   
